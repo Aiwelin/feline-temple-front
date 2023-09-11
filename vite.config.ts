0de5cdf0556816c55de4ea2f5b3771875f8ebc80
Pichard Amandine <pichard.amandine@gmail.com>
2023-09-11 19:06:45 +0200

    Modification des fichiers pour permettre que le build passe sans warning erreurs

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,20 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: envWithProcessPrefix,
     base: base,
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          },
+        },
+      },
+    },
   };
 });
